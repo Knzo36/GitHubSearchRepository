453882e0a5d3012b7b8140826494bfdc8a8cd4cb
knzo36 <ken.takagi216@gmail.com>
2020-03-29 17:15:21 +0900

    first commit

---
+++ GitHubSearchRepositoryTest/GitHubSearchRepositoryTest.swift
@@ -0,0 +1,33 @@
+//
+//  GitHubSearchRepositoryTest.swift
+//  GitHubSearchRepositoryTest
+//
+//  Created by 高木 健三朗 on 2020/03/29.
+//  Copyright © 2020 高木 健三朗. All rights reserved.
+//
+
+import XCTest
+
+class GitHubSearchRepositoryTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
