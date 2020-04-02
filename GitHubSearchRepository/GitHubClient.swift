f67fba7b3ce0df2f016e5181014fbab88be2054e
knzo36 <ken.takagi216@gmail.com>
2020-04-02 21:05:51 +0900

    fix

--- GitHubSearchRepository/GitHubClient.swift
+++ GitHubSearchRepository/GitHubClient.swift
@@ -17,7 +17,8 @@ class GitHubClient {
     
     func send<Request : GitHubRequest>(
         request: Request,
-        completion: @escaping (Result<Request.Response, GitHubClientError>) -> Void) {
+        completion: @escaping (Result<Request.Response, GitHubClientError>) -> Void)
+    {
         let urlRequest = request.buildURLRequest()
         let task = session.dataTask(with: urlRequest) {
             data, response, error in
@@ -28,13 +29,14 @@ class GitHubClient {
             case(let data?, let response?, _):
                 do {
                     let response = try request.response(from: data, urlResponse: response)
+                    completion(Result(value: response))
                 } catch let error as GitHubAPIError {
                     completion(Result(error: .apiError(error)))
                 } catch {
                     completion(Result(error: .responseParseError(error)))
                 }
             default:
-                fatalError("invalid response combination \(data), \(response), \(error)")
+                fatalError("invalid response combination \(data), \(response), \(error).")
             }
         }
         
